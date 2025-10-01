17e0d6199d7995bfad70366972369579e15b491d
Rayyan-Asia <RayyanTawfieg@Gmail.com>
2025-10-01 16:45:44 +0300

    Setup entities correctly

--- src/main/java/rayyan/asia/domain/entities/Invoice.java
+++ src/main/java/rayyan/asia/domain/entities/Invoice.java
@@ -1,12 +1,16 @@
 package rayyan.asia.domain.entities;
 
 import org.bson.types.ObjectId;
+import org.springframework.data.annotation.Id;
+import org.springframework.data.mongodb.core.mapping.Document;
 
 import lombok.Data;
 import lombok.experimental.Accessors;
 
 @Data
 @Accessors(chain = true)
+@Document(collection = "invoices")
 public class Invoice {
+    @Id
     private ObjectId id;
 }
--- src/main/java/rayyan/asia/domain/entities/Order.java
+++ src/main/java/rayyan/asia/domain/entities/Order.java
@@ -1,12 +1,16 @@
 package rayyan.asia.domain.entities;
 
 import org.bson.types.ObjectId;
+import org.springframework.data.annotation.Id;
+import org.springframework.data.mongodb.core.mapping.Document;
 
 import lombok.Data;
 import lombok.experimental.Accessors;
 
 @Data
 @Accessors(chain = true)
+@Document(collection = "orders")
 public class Order {
+    @Id
     private ObjectId id;
 }
